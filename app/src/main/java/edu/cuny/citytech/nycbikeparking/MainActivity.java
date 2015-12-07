b40e1cdd19ca1dcba7fdb8123d0df3d81c869576
Raffi Khatchadourian <rkhatchadourian@citytech.cuny.edu>
2015-12-07 11:50:05 -0500

    I don't think I need a compatible activity.

--- app/src/main/java/edu/cuny/citytech/nycbikeparking/MainActivity.java
+++ app/src/main/java/edu/cuny/citytech/nycbikeparking/MainActivity.java
@@ -1,14 +1,14 @@
 package edu.cuny.citytech.nycbikeparking;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     public static final String LOCATION_KEY = "edu.cuny.citytech.LOCATION_KEY";
 
